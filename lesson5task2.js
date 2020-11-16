57593cb51447020c28606a6e89beb558499ad1a0
Unknown <max007446@gmail.com>
2020-11-16 23:45:12 +0300

    task2 не очищается таблица

--- lesson5task2.js
+++ lesson5task2.js
@@ -1,15 +1,62 @@
-'use strict'
-/*let basket={
+
+const cartTable = {
+    render(good) {
+       return   `<div class="good"><div> <b> Наименование</b>: ${good.product_name}</div><div><b>Цена за шт.</b>: ${good.price}</div><div><b>Количество</b>: ${good.quantity}</div> <div><b>Стоимость</b>: ${good.quantity * good.price}</div></div>`;
+    }
+}
+const basket = {
+    
+    cartClearButton: null, 
+    cartCell: null, 
+    cartTable,
+    
     goods: [
-        {name : "карандаш",
-        price: 500};
-        {name : "ручка",
-        price: 100}
+        {
+            product_name: 'карандаш',
+            price: 500, 
+            quantity: 5,
+        }, 
+        {
+            product_name: 'ручка',
+            price: 10, 
+            quantity: 20, 
+        }, 
+        {
+            product_name: 'блокнот', 
+            price: 150,
+            quantity: 2,
+        }, 
+        {
+            product_name: 'ластик', 
+            price: 50, 
+            quantity: 2, 
+        },
     ],
-    getSum(this.goods),
-        
-}*/
-document.body.insertAdjacentHTML('afterbegin','<div> max </div>');
-let table1 = document.createElement('table');
-//document.querySelector('table').insertAdjacentHTML('afterbegin','<tr><td> 1 </td><td> 2 </td></tr>');
-console.log(document.querySelector('table'));
+    getSumCart() {
+        return this.goods.reduce(function (price, good) {
+            return price + good.price * good.quantity;
+        }, 0);
+    }, 
+    init() {
+        this.cartCell = document.querySelector('.cart');
+        this.cartClearButton = document.querySelector('.clear-btn');
+//        this.cartClearButton.addEventListener('click', this.clearCart.bind(this));
+        this.render();
+    },
+    render() {
+        if (this.goods.length) {
+            this.goods.forEach(good => {
+                this.cartCell.insertAdjacentHTML('beforeend', this.cartTable.render(good));
+            });
+            this.cartCell.insertAdjacentHTML('beforeend', `В корзине ${this.goods.length} позиций(а) стоимостью ${this.getSumCart()}`);
+        }
+        else {
+            this.cartCell.textContent = 'Корзина пуста';
+        }
+    },
+    clearCart() {
+        this.goods = [];
+        this.render();
+    },
+};
+basket.init();
