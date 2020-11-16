48fd23f28351d598c378b54f25523c828cc927e4
Max007446 <Max007446@gmail.com>
2020-11-16 14:50:12 +0300

    Task2

---
+++ lesson5task2.js
@@ -0,0 +1,15 @@
+'use strict'
+/*let basket={
+    goods: [
+        {name : "карандаш",
+        price: 500};
+        {name : "ручка",
+        price: 100}
+    ],
+    getSum(this.goods),
+        
+}*/
+document.body.insertAdjacentHTML('afterbegin','<div> max </div>');
+let table1 = document.createElement('table');
+//document.querySelector('table').insertAdjacentHTML('afterbegin','<tr><td> 1 </td><td> 2 </td></tr>');
+console.log(document.querySelector('table'));
